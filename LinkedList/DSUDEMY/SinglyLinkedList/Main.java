803ce7de43724a9120555b9b744e2528f603a5aa
shubham singh <singhshubham68738@gmail.com>
2025-10-05 23:07:17 +0530

    working again

--- LinkedList/DSUDEMY/SinglyLinkedList/Main.java
+++ LinkedList/DSUDEMY/SinglyLinkedList/Main.java
@@ -1,6 +1,16 @@
 package LinkedList.DSUDEMY.SinglyLinkedList;
 
 public class Main {
+    public static LinkedListDemo.Node findMiddle(LinkedListDemo list) {
+        LinkedListDemo.Node fast = list.head;
+        LinkedListDemo.Node slow = list.head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
     public static void main(String[] args) {
         LinkedListDemo list1 = new LinkedListDemo();
         list1.append(1);
@@ -12,5 +22,7 @@ public class Main {
         list1.printList();
         list1.insertAt(2, 5);
         list1.printList();
+        LinkedListDemo.Node middle = findMiddle(list1);
+        System.out.println("\n" + middle.data);
     }
 }
